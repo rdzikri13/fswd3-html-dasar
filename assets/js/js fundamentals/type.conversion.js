0996b1294fcb03ccb7407636de08571230d43fed
rdzikri13 <126127985+rdzikri13@users.noreply.github.com>
2023-03-18 11:44:31 +0700

    Add files via upload

---
+++ assets/js/js fundamentals/type.conversion.js
@@ -0,0 +1,20 @@
+//TYPE CONVERSION STRING
+// value = String(value); // now value is a string "true"
+// alert(typeof value); // string
+
+// TYPE CONVERSION NUMERIC
+// alert("6" / "2"); // 3, strings are converted to numbers
+
+// let str = "123";
+// alert(typeof str); // string
+
+// let num = Number(str); // becomes a number 123
+
+// alert(typeof num); // number
+
+//TYPE CONVERSION BOOLEAN
+alert(Boolean(1)); //true
+alert(Boolean(0)); //false
+
+alert(Boolean("hello")); //true
+alert(Boolean("")); //false
