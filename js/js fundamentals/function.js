86a9b174e6b4701ba661e61530c3b2d363b87904
rdzikri13 <126127985+rdzikri13@users.noreply.github.com>
2023-03-18 11:42:19 +0700

    Add files via upload

---
+++ js/js fundamentals/function.js
@@ -0,0 +1,21 @@
+//BASIC FUNCTION
+function salam() {
+    console.log("Hello World!");  
+}
+
+salam();
+
+//FUNCTION EXPRESSION
+//let salam = function () {
+//    console.log("Hello World!");
+//}
+//salam();
+
+//ARROW FUNCTION
+//var salam = () => {
+//    console.log("Hello World!");
+//}
+//var salam2 = () => console.log("Hello World!");
+
+//salam();
+//salam2();
---
+++ js/js fundamentals/struktur.kode.js
@@ -0,0 +1,14 @@
+//statement
+console.log("Welcome to my page");
+alert("Welcome to my page");
+
+console.log("Welcome to my page");
+alert("Welcome to my page");
+
+// Ini adalah komentar satu baris
+
+/* Ini adalah komentar
+multi
+baris*/
+
+console.log("Welcome to my page");
